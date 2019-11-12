465d3104713c130cd02f6f9bce32649bd201874f
Mikhail Abbas <mikhail.abbas@gmail.com>
2019-11-12 21:30:03 +0800

    Added TODO list
    Things to do in the program, mate.

--- Assignment_1.py
+++ Assignment_1.py
@@ -1,7 +1,16 @@
-class EmergencyRoomQueue:
-  def main():
-    print("Starting program....")
+# Import python libraries
 
-  
+# Main program function
+def main():
+  print("Starting program....")
 
-  main()
+# TODO Request patient name
+# TODO Request patient priority
+# TODO Assign timestamp for patient
+# TODO Sort patient list by priority
+# TODO Sort new patient list by timestamp
+# TODO Display patient list
+# TODO Append patient record (name & priority) to a file
+# TODO Remove names upon treatment completion
+
+main()
